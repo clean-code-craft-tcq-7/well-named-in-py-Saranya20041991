a614c3faf8ed5930e4f970cb4a62d4539ced42dd
Saranya <saranyaacbe@yahoo.com>
2025-08-12 16:04:27 +0530

    created separate file for test and main

--- test_colormap.py
+++ test_colormap.py
@@ -1,3 +1,5 @@
+from color_conversion import get_color_from_pair_number,get_pair_number_from_color
+
 def test_number_to_pair(pair_number,
                         expected_major_color, expected_minor_color):
   major_color, minor_color = get_color_from_pair_number(pair_number)
